c2795219709e591593a26fad0c8d759658e15b43
aidenwaring <aiden.waring@icloud.com>
2020-07-16 12:12:44 +1000

    Filled in demo contact info for component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Contact() {
     return (
         <div>
-            Contact Us
+            <h1>Get In Touch</h1>
+            <p>123 Mockup St, New York</p>
+            <p>Ph: (+1) 123 456 789</p>
+            <p>Em: hello@equipped.com.au</p>
+            <form>
+                Hello!
+            </form>
         </div>
     )
 }
