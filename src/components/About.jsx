11052791dc9478b8b32320410f2c1f8d1965ae85
Alex <alittle1092@gmail.com>
2020-07-21 13:34:05 +1000

    95% complete trying to deploy now

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,32 @@
 import React from "react";
-
+import { FiArrowDownCircle } from "react-icons/fi";
 import { Container } from "react-bootstrap";
+import Footer from "./footer.jsx";
 
 export default function About() {
   return (
-    <Container className="about">
-      <div className="justify-content-md-center">
-        <h1>About Us</h1>
-        <br></br>
-        <h3>
-          We 'equip' those you need tools with those who can provide them.
-          <br />
-          <br />
-          This is our mission.
-        </h3>
-      </div>
-      <div>
-        <h5>Who are we?</h5>
-      </div>
-    </Container>
+    <div className="about">
+      <Container className="about-container">
+        <div className="hero-text">
+          <h1>About Us</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            ipsa
+          </p>
+          <FiArrowDownCircle className="down-arrow" />
+        </div>
+      </Container>
+
+      <Container className="mission">
+        <h2>Our Mission</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat,
+          eligendi alias. Magnam consequuntur quae consequatur reprehenderit?
+          Hic praesentium animi placeat?
+        </p>
+      </Container>
+
+      <Footer />
+    </div>
   );
 }
--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -1,53 +1,125 @@
-import React, { useState } from 'react';
-
-import Carousel from 'react-bootstrap/Carousel'
+import React, { useState } from "react";
+import { RiMailSendLine } from "react-icons/ri";
+import { MdReceipt } from "react-icons/md";
+import { FiArrowDownCircle, FiThumbsUp } from "react-icons/fi";
+import { Container, Form, Button, Card } from "react-bootstrap";
+import Footer from "./footer";
 
 export default function HomeView() {
-    const [index, setIndex] = useState(0);
-  
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    };
-  
-    return (
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    );
-  }
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div className="home-view">
+      <Container className="home-text">
+        <div className="hero-text">
+          <h1> Rent The Things You Need</h1>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="What do you need" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="How long do you need it"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className>
+              Submit
+            </Button>
+          </Form>
+          <FiArrowDownCircle className="down-arrow" />
+        </div>
+      </Container>
+
+      <Container className="home-1">
+        <Card>
+          <Card.Body>
+            <Card.Title style={{ color: "black" }}>
+              Find the equipment your need
+            </Card.Title>
+            <Card.Text style={{ color: "black" }}>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Title style={{ color: "black" }}>Free to use</Card.Title>
+            <Card.Text style={{ color: "black" }}>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Title style={{ color: "black" }}>
+              Save Time & Money
+            </Card.Title>
+            <Card.Text style={{ color: "black" }}>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Container>
+
+      <Container className="home-2">
+        <h3>How Equipped Works</h3>
+
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <RiMailSendLine style={{ fontSize: "3em" }} />
+            </Card.Title>
+            <Card.Title style={{ color: "white" }}>Request a quote</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <MdReceipt style={{ fontSize: "3em" }} />
+            </Card.Title>
+            <Card.Title style={{ color: "white" }}>Receive Quotes</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <FiThumbsUp style={{ fontSize: "3em" }} />
+            </Card.Title>
+            <Card.Title style={{ color: "white" }}>
+              Choose The Best Quote
+            </Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Container>
+      <Footer />
+    </div>
+  );
+}
