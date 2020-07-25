98442d1a0951c976b4916164f24a6b76c727c694
Alex <alittle1092@gmail.com>
2020-07-25 13:52:28 +1000

    fixing passport

--- src/components/registration/SignIn.jsx
+++ src/components/registration/SignIn.jsx
@@ -1,31 +1,54 @@
-import React, {useState, UseEffect} from 'react'
+import React, { useState, UseEffect } from "react";
 import { Button, Form, Container } from "react-bootstrap";
-import axios from 'axios'
+import axios from "axios";
 
 
-export default function SignIn() {
-
-// url for user crud 
-const url = "https://secure-savannah-73387.herokuapp.com/api/users/"
-
-// State for user
-const [user, setUser] = useState(false)
-const [error, setError] = useState(false)
+export default function SignIn({setUser}) {
 
 
-// Google Oauth
-const handleGoogleAuth = (e) => {
-  window.location = `${url}auth/google`
-}
+  // url for user crud
+  const url = "http://localhost:5000/api/users";
+
+  // State for user
+  const [error, setError] = useState(false);
+
+  const handleLogIn = (e) => {
+    e.preventDefault();
+  
+    axios
+      .post(
+        `${url}/login`,
+        {
+          username: 'alex123',
+          password: 'alex123',
+        },
+        {
+          withCredentials: true,
+        }
+      ).then(
+        response => console.log(response.data)
+      )
+     
+      .catch((err) => {
+        // setError(err.response.data);
+      });
+  };
+
+  // Google Oauth
+  const handleGoogleAuth = (e) => {
+    e.preventDefault()
+    console.log(url)
+    window.location = `${url}/auth/google`;
+  };
 
   return (
     <div>
       <Container>
         <h1>Sign In</h1>
-        <Form>
+        <Form onSubmit={handleLogIn}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control  placeholder="Enter email" />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
@@ -40,9 +63,14 @@ const handleGoogleAuth = (e) => {
           </Button>
         </Form>
       </Container>
-
+      {error ? (
+        <div>
+          <h4>{error.name}</h4>
+          <p>{error.message}</p>
+        </div>
+      ) : null}
       <Container>
-      <Button onClick={handleGoogleAuth}>Log In With Google!</Button>
+        <Button onClick={handleGoogleAuth}>Log In With Google!</Button>
       </Container>
     </div>
   );
