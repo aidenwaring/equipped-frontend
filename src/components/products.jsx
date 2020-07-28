95c2169e0f577ed727231727812e9efcb7de9ff7
aidenwaring <aiden.waring@icloud.com>
2020-07-28 20:39:36 +1000

    Implemented correct product image syntax for product list mapping. Updated myproducts and products pages.

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -2,12 +2,11 @@ import React, { useEffect } from "react";
 import { Container, Button, Card, CardDeck } from "react-bootstrap";
 import { FiArrowDownCircle } from "react-icons/fi";
 import Footer from "./footer.jsx";
-import axios from 'axios'
-//Axios
-// import axios from 'axios'
+import axios from "axios";
+import { Image } from "cloudinary-react";
+import { CloudinaryContext, Transformation } from "cloudinary-react";
 
-export default function Products({product, setProduct}) {
-
+export default function Products({ product, setProduct }) {
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/products", { withCredentials: true })
@@ -20,11 +19,7 @@ export default function Products({product, setProduct}) {
       });
   }, []);
 
-
-
-
   return (
-    
     <div className="products">
       <Container className="products-main">
         <div className="hero-text">
@@ -39,12 +34,16 @@ export default function Products({product, setProduct}) {
       </Container>
 
       <Container>
-        
         <CardDeck>
           {product.map((product) => (
-
             <Card style={{ minWidth: "14rem" }}>
-              <Card.Img variant="top" src="{product.image}" />
+              <Image
+                cloudName="dgeizgzdw"
+                publicId={`equipped/${product._id}`}
+                width="200"
+                height="200"
+                crop="scale"
+              />
               <Card.Body>
                 <Card.Title>{product.product}</Card.Title>
                 <Button variant="primary">Go somewhere</Button>
@@ -53,7 +52,6 @@ export default function Products({product, setProduct}) {
           ))}
         </CardDeck>
       </Container>
-      
       <Footer />
     </div>
   );
--- src/components/products/myProducts.jsx
+++ src/components/products/myProducts.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { Card, Button, CardDeck, Container } from "react-bootstrap";
 
-import {Image} from 'cloudinary-react';
-import {CloudinaryContext, Transformation} from 'cloudinary-react';
+import { Image } from "cloudinary-react";
+import { CloudinaryContext, Transformation } from "cloudinary-react";
 
 function MyProducts({ myProducts, setMyProducts }) {
   useEffect(() => {
@@ -20,21 +20,18 @@ function MyProducts({ myProducts, setMyProducts }) {
 
   return (
     <div>
-    <Image cloudName="demo" publicId="sample" />
-    <Image cloudName="dgeizgzdw" publicID="" width="300" height="300" crop="scale" />
-    <CloudinaryContext cloudName="dgeizgdw">
-      <Image publicId="5f1fbe2c5b118b8e46596ee1">
-          <Transformation width="200" crop="scale" angle="10"/>
-      </Image>
-    </CloudinaryContext>
-
       <Container>
         <h2>My Products</h2>
         <CardDeck>
           {myProducts.map((product) => (
-
             <Card style={{ minWidth: "14rem" }}>
-              <Card.Img variant="top" src="{product.image}" />
+              <Image
+                cloudName="dgeizgzdw"
+                publicId={`equipped/${product._id}`}
+                width="200"
+                height="200"
+                crop="scale"
+              />
               <Card.Body>
                 <Card.Title>{product.product}</Card.Title>
                 <Button variant="primary">Go somewhere</Button>
