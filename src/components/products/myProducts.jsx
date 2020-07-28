5ab334568167962ce540eb8782371e2469bc2305
aidenwaring <aiden.waring@icloud.com>
2020-07-28 16:21:49 +1000

    Installed Cloudinary-React. Setting up demo display image

--- src/components/products/myProducts.jsx
+++ src/components/products/myProducts.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { Card, Button, CardGroup, Container } from "react-bootstrap";
 
+import {Image} from 'cloudinary-react';
+import {CloudinaryContext, Transformation} from 'cloudinary-react';
+
 function MyProducts({ myProducts, setMyProducts }) {
   useEffect(() => {
     axios
@@ -17,13 +20,20 @@ function MyProducts({ myProducts, setMyProducts }) {
 
   return (
     <div>
+    <Image cloudName="demo" publicId="sample" />
+    <Image cloudName="dgeizgzdw" publicID="" width="300" height="300" crop="scale" />
+    <CloudinaryContext cloudName="dgeizgdw">
+      <Image publicId="5f1fbe2c5b118b8e46596ee1">
+          <Transformation width="200" crop="scale" angle="10"/>
+      </Image>
+    </CloudinaryContext>
+
       <Container>
         <h2>My Products</h2>
         <CardGroup>
           {myProducts.map((product) => (
-
             <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="{product.image}" />
+              <Card.Img variant="top" src="https://res.cloudinary.com/dgeizgzdw/image/upload/v1595901234/equipped/undefined.jpg" />
               <Card.Body>
                 <Card.Title>{product.product}</Card.Title>
                 <Button variant="primary">Go somewhere</Button>
